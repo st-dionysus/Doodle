12bf41cca5575ee9b6b23a5efe975701f35e00f8
st-dionysus <37880480+st-dionysus@users.noreply.github.com>
2020-06-19 14:19:17 +0300

    init

---
+++ DoodleJump/Assets/C#/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform doodlePos; 
+
+    void Update()
+    {
+        if (doodlePos.position.y > transform.position.y) 
+        {
+           
+            transform.position = new Vector3(transform.position.x, doodlePos.position.y, transform.position.z); 
+        }
+    }
+
+    
+    
+}
---
+++ DoodleJump/Assets/C#/Doodle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;                          
+
+public class Doodle : MonoBehaviour
+{
+ 
+
+    public static Doodle instance;                          
+
+    float horizontal;                                       
+    public Rigidbody2D DoodleRigid;
+    
+
+    void Start()
+    {
+        if (instance == null)                               
+        {
+            instance = this;                               
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            horizontal = Input.acceleration.x;
+        }
+
+        if (Input.acceleration.x < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (Input.acceleration.x > 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+
+       
+        DoodleRigid.velocity = new Vector2(Input.acceleration.x * 10f, DoodleRigid.velocity.y);     
+    }
+
+
+    public void OnCollisionEnter2D(Collision2D collision)       
+    {
+        if (collision.collider.name == "DeadZone")              
+        {
+            SceneManager.LoadScene(0);                          
+        }
+    }
+
+    
+    
+}
